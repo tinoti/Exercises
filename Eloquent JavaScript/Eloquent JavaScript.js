8a24f20906753d2999ae64849b31b5e486b6272a
tinoti <valentino.tomasic@gmail.com>
2019-01-01 23:39:31 +0100

    Add 3.1, 3.2 and 3.3 Function exercises

--- Eloquent JavaScript/Eloquent JavaScript.js
+++ Eloquent JavaScript/Eloquent JavaScript.js
@@ -58,3 +58,66 @@ for(let i = 0; i < size; i++){
   }
   string+="\n";
 }
+
+
+/* 3.1 - Minimum
+ The previous chapter introduced the standard function Math.min that returns its smallest argument. We can build
+something like that now. Write a function min that takes two arguments and returns their minimum. */
+
+function min(a, b){
+  if(a < b) return a;
+  else return b;
+};
+
+
+/* 3.2 - Recursion
+We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to see
+whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
+Zero is even.
+One is odd.
+For any other number N, its evenness is the same as N - 2.
+Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a
+positive, whole number) and return a Boolean.
+Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this? */
+
+function isEven(number) {
+  number = Math.abs(number);
+  if(number === 0) return true;
+  else if(number === 1) return false;
+  else return isEven(number - 2);
+}
+console.log(isEven(-7));
+
+
+
+/* 3.3 - Bean counting
+ You can get the Nth character, or letter, from a string by writing "string"[N]. The returned value will be a string
+containing only one character (for example, "b"). The first character has position 0, which causes the last one to be
+found at position string.length - 1. In other words, a two-character string has length 2, and its characters have
+positions 0 and 1.
+
+Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase
+“B” characters there are in the string.
+
+Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the
+character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of
+this new function. */
+
+
+function countBs(string){
+  let counter = 0;
+  for(let i = 0; i < string.length; i++){
+    if(string[i] === "B") counter++;
+  }
+  return counter;
+}
+
+function countChar(string, letter) {
+  let counter = 0;
+  for(let i = 0; i < string.length; i++) {
+    if(string[i] === letter) counter++;
+  }
+  return counter;
+}
+
+console.log(countChar("BBCDCC", "c"));
