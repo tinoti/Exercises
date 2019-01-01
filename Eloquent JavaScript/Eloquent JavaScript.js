a8d81e6bbcd0329a155c42cefec4a06f9fc307fb
tinoti <valentino.tomasic@gmail.com>
2019-01-01 20:38:31 +0100

    Add 2.1 Looping a triangle exercise

---
+++ Eloquent JavaScript/Eloquent JavaScript.js
@@ -0,0 +1,16 @@
+/* 2.1 Looping a triangle
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
+
+for(let hashtag = "#"; hashtag.length < 8; hashtag+="#"){
+  console.log(hashtag);
+}
