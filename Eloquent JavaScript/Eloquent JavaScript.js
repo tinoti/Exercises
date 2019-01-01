1d1cbcef97af66ffc892873502a19ad03322f894
tinoti <valentino.tomasic@gmail.com>
2019-01-01 22:32:44 +0100

    Add 2.3 - Chessboard exercise

--- Eloquent JavaScript/Eloquent JavaScript.js
+++ Eloquent JavaScript/Eloquent JavaScript.js
@@ -27,3 +27,34 @@ for(let i =  1; i <= 100; i++){
   else if ( i % 5 === 0 && !(i % 3 === 0)) console.log("Buzz");
   else console.log(i);
 }
+
+
+/* 2.3 - Chessboard
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each
+position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+Passing this string to console.log should show something like this:
+ # # # #
+# # # #
+ # # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+
+When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works
+for any size, outputting a grid of the given width and height. */
+
+
+let size = 15;
+let string = "";
+for(let i = 0; i < size; i++){
+
+  for (let j = 0; j < size; j++){
+    if(( (i % 2 === 0 && j % 2 === 0) || (i % 2 != 0 && j % 2 != 0))){
+      string+= " ";
+    }
+    else string+= "#";
+  }
+  string+="\n";
+}
