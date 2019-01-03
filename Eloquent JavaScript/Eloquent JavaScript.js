8cb025013d0a262fc1d503bf6a580e38a0031dad
tinoti <valentino.tomasic@gmail.com>
2019-01-03 21:14:27 +0100

    Add Squirrelification exercise

--- Eloquent JavaScript/Eloquent JavaScript.js
+++ Eloquent JavaScript/Eloquent JavaScript.js
@@ -121,3 +121,70 @@ function countChar(string, letter) {
 }
 
 console.log(countChar("BBCDCC", "c"));
+
+
+/*  Taken from Data Structures: Objects and Arrays.
+    Try to figure out the solution myself with as little help as possible.
+
+******* PSEUDO - CODE *****
+
+Figure out what causes the squirrelification
+    1. Write a program that finds a correlation between the specific event and squirrelification
+      1.1. Write a function that takes event and journal  and counts how many times each of the possible 4 variants
+       occurs. Function takes event and journal, returns array with 4 indexes, each index has the number of
+       instances occured
+      E.G event is pizza, function returns array[76, 9, 4, 1]
+
+      1.2. Write a function that takes the array from the first function adn applies the correlation formula,
+      returns the coeficient.
+
+      1.3. Make a list of every event and loop through it with the two functions
+        1.3.1 List of every event - loop through each day and every event in the day, compare it to event array,
+        if there's a event that matches go to next, otherwise add it to array.
+
+    2. Isolate instances of higher correlation and expand on it
+    *******     ********
+*/
+function correlate(event, journal) { //counts how many of each of 4 possible variants there is.
+  let table = [0,0,0,0];
+  for(let i = 0; i < journal.length; i++) {
+    let events = journal[i], index = 0;
+    if(events.events.includes(event)) index+= 1;
+    if(events.squirrel) index+= 2;
+    table[index] += 1;
+  }
+  return table;
+}
+
+function phi(table) { // calculates the correlation (-1, 0, 1) between two events from the table given by correlate
+return (table[3] * table[0] - table[2] * table[1]) /
+Math.sqrt((table[2] + table[3]) *
+          (table[0] + table[1]) *
+          (table[1] + table[3]) *
+          (table[0] + table[2]));
+}
+
+function eventList(journal) { // loops through each event and puts it in array if it's not already there
+let events = [];
+for(let entry of journal) {
+  for(let event of entry.events) {
+    if (!events.includes(event)) events.push(event);
+  }
+}
+return events;
+}
+
+
+let events = eventList(JOURNAL);
+
+for(let event of events){ // outputs all events that have more than 0.1 or -0.1 correlation
+  let correlation = phi(correlate(event,JOURNAL));
+  if(correlation > 0.1 || correlation < -0.1) console.log(event + ": " + correlation);
+}
+
+for(let entry of JOURNAL) { // "adds peanut teeth" event on each event that has peanuts and doesn't have brushed
+  if(entry.events.includes("peanuts") && //teeth (used primarly so we can test correlation on console.log below);
+    !entry.events.includes("brushed teeth")) entry.events.push("peanut teeth");
+}
+
+console.log(phi(correlate("peanut teeth", JOURNAL))); // tests for correlation, result is 1
