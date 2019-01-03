f17120eb3c3e3b9b33d4b882ce4955ebb07d974f
tinoti <valentino.tomasic@gmail.com>
2019-01-03 22:08:01 +0100

    Add 4.1 - The Sum of a Range exercise

--- Eloquent JavaScript/Eloquent JavaScript.js
+++ Eloquent JavaScript/Eloquent JavaScript.js
@@ -310,3 +310,50 @@ console.log(phi(correlate("peanut teeth", JOURNAL))); // tests for correlation, 
 
 /*****************************************************************************************************************/
 /*****************************************************************************************************************/
+
+
+/*                                          4.1 - The Sum of a Range
+
+The introduction of this book alluded to the following as a nice way to compute the sum of a range of numbers:
+
+  console.log(sum(range(1, 10)));
+
+Write a range function that takes two arguments, start and end, and returns an array
+containing all the numbers from start up to (and including) end.
+
+Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program
+and see whether it does indeed return 55.
+
+As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value
+used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old
+behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step
+values so that range(5, 2, -1) produces [5, 4, 3, 2]. */
+
+function range(a, b, step) {
+  let array = [];
+  if(step && step < 0) {
+    while(a >= b) {
+      array.push(a);
+      a+= step;
+    }
+    return array;
+  }
+  while(a <= b) {
+     array.push(a);
+     if(step) a+= step;
+     else a++;
+  }
+  return array;
+}
+
+function sum(array) {
+  let sum = 0;
+  for(let number of array) {
+    sum+= number;
+  }
+  return sum;
+}
+console.log(sum(range(1,10)));
+
+/*****************************************************************************************************************/
+/*****************************************************************************************************************/
